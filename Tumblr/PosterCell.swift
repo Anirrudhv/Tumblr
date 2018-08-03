9cdf56a395854a3f7cb2922812a6429b2fa2de08
Anirudh V <anirudhv@Anirudhs-MacBook-Pro.local>
2018-08-02 21:11:37 -0700

    Part2 done

---
+++ Tumblr/PosterCell.swift
@@ -0,0 +1,24 @@
+//
+//  PosterCell.swift
+//  Tumblr
+//
+//  Created by Anirudh V on 8/2/18.
+//  Copyright © 2018 Anirudh V. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
